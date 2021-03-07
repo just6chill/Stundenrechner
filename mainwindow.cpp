8e05c9bba0f990155455aff5d97be778750895b4
just6chill <54991398+just6chill@users.noreply.github.com>
2021-03-07 13:32:30 +0100

    added comments and more useful names for variables

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,34 +22,56 @@ void MainWindow::on_pushButton_clicked()
     char *token;
     char dot[2] = ".";
 
-    int hours = ui->spinBox->value();
-    int minutes = ui->spinBox_2->value();
-    int pauses = ui->spinBox_3->value();
-    float flength = ui->doubleSpinBox->value();
+    //claim values from the input boxes
+    int IntegerHours = ui->spinBox->value();
+    int IntegerMinutes = ui->spinBox_2->value();
+    int IntegerPause = ui->spinBox_3->value();
+    float FloatLength = ui->doubleSpinBox->value();
 
-    float fhours = (float) hours;
-    float fminutes = (float) minutes;
-    float fpauses = (float) pauses;
-    float ftime = fhours + (fminutes / 60) + (pauses /60);
+    //cast the integers to float ((f)hours etc)
+    float FloatHours = (float) IntegerHours;
+    float FloatMinutes = (float) IntegerMinutes;
+    float FloatPause = (float) IntegerPause;
 
-    ftime = ftime + flength;
-    char ctime[10];
-    snprintf(ctime, sizeof(ctime), "%0.4f\n", ftime);
-    token = strtok(ctime, dot);
-    char *hoursn;
-    hoursn = token;
+    //calculate the time to decimal hours
+    float FloatTime = FloatHours + (FloatMinutes / 60) + (FloatPause /60);
+    FloatTime = FloatTime + FloatLength;
+
+    //convert the decimal hours float to a char
+    char CharTime[10];
+    snprintf(CharTime, sizeof(CharTime), "%0.4f\n", FloatTime);
+
+    //split the hours away from the char string and call it hours(n)
+    token = strtok(CharTime, dot);
+    char *CharHours;
+    CharHours = token;
+
+    //split the minutes away from the char string and call it one
     token = strtok(NULL, dot);
-    char *one;
-    one = token;
-    char minuten[10] = "0.";
-    strcat(minuten, one);
+    char *PartOfCharMinutes;
+    PartOfCharMinutes = token;
 
-    float minutef = atof(minuten);
-    minutef = minutef * 60;
-    int minute = (int) minutef;
-    int hour = atoi(hoursn);
-    QString text = QString("Uhrzeit %1:").arg(hour);
-    text.append(QString::number(minute));
+    //put a 0. in front of the minutes char
+    char CharMinutes[10] = "0.";
+    strcat(CharMinutes, PartOfCharMinutes);
 
+    //convert the minutes char with 0. in its front to a float,
+    //and call it minute(f)
+    float FinalMinutes = atof(CharMinutes);
+
+    //calculate the real minutes out from decimal
+    FinalMinutes = FinalMinutes * 60;
+
+    //cast the minutes float to a integer
+    int FinalIntMinutes = (int)FinalMinutes;
+
+    //convert the hours char to a integer
+    int FinalIntHours = atoi(CharHours);
+
+    //prepare output string (hours:minutes)
+    QString text = QString("Uhrzeit %1:").arg(FinalIntHours);
+    text.append(QString::number(FinalIntMinutes));
+
+    //output the result
     ui->lineEdit->setText(text);
 }
